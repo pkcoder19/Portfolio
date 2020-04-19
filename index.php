a0e06483fc0ab469480a4980fcb14b6ca428ccf1
pkcoder19 <57236616+pkcoder19@users.noreply.github.com>
2020-04-19 13:26:24 -0700

    Added files from Replit portfolio

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Pradnya Kadam Period:6]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Kadam_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Kadam_Projects.html">Projects.html</a></li></br>
+      <li><a href="Kadam_Resume.html">Resume.html</a></li></br>
+      <li><a href="Kadam_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
